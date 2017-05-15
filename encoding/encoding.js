eab0e8bc596614c69a2fa996b194864ce6f5fc34
Sebastian Apel <Seb@dyn-0a230c36.fmi.uni-jena.de>
2017-05-15 18:34:34 +0200

    added new property to select specific field to transform encoding

--- encoding/encoding.js
+++ encoding/encoding.js
@@ -6,15 +6,36 @@ module.exports = function(RED) {
         function isBuffer (obj) {
           return !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
         }
+
+        this.field = config.field || "payload";
+        this.fieldType = config.fieldType || "msg";
+
         this.on('input', function (msg) {
+          // Read property
+          var text = "";
+          if (node.fieldType === 'msg' && msg) {
+            text = RED.util.getMessageProperty(msg, node.field);
+          } else if (node.fieldType === 'flow' && node) {
+            text = node.context().flow.get(node.field);
+          } else if (node.fieldType === 'global' && node) {
+            text = node.context().global.get(node.field);
+          }
           var toCharset = config.destination || msg.destination;
           var fromCharset = config.source || msg.source;
-          var text = msg.payload;
           try {
+            // Encode
             if(isBuffer(text)) {
-              msg.payload = encoding.convert(text, toCharset, fromCharset);
+              text = encoding.convert(text, toCharset, fromCharset);
             } else {
-              msg.payload = encoding.convert(Buffer.from(text, fromCharset), toCharset, fromCharset).toString(toCharset);
+              text = encoding.convert(Buffer.from(text + "", fromCharset), toCharset, fromCharset).toString(toCharset);
+            }
+            // Save to msg, flow or global
+            if (node.fieldType === 'msg' && msg) {
+              RED.util.setMessageProperty(msg, node.field, text);
+            } else if (node.fieldType === 'flow' && node) {
+              node.context().flow.set(node.field, text);
+            } else if (node.fieldType === 'global' && node) {
+              node.context().global.set(node.field, text);
             }
           } catch(e) {
             this.error("Can't convert from " + fromCharset + " to " + toCharset + ": " + e);
